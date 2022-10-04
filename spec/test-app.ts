fcdf4627a3f04174e452b8c0f9c8181fd504e87c
Kobe Li <likobe23@gmail.com>
2022-10-03 18:00:58 -0700

    Add test files

---
+++ spec/test-app.ts
@@ -0,0 +1,21 @@
+import { CharStreams, CommonTokenStream } from 'antlr4ts';
+import fs from 'node:fs';
+import path from 'node:path';
+import { ParserToASTConverter } from '../src/ast/ParserToASTConverter';
+import { EZDiscordLexer } from '../src/parser/EZDiscordLexer';
+import { EZDiscordParser } from '../src/parser/EZDiscordParser';
+
+// test input
+const testInput = fs.readFileSync(path.resolve("spec/testInput.txt")).toString();
+
+const charStream = CharStreams.fromString(testInput);
+const lexer = new EZDiscordLexer(charStream);
+const tokenStream = new CommonTokenStream(lexer);
+const parser = new EZDiscordParser(tokenStream);
+
+const tree = parser.bot();
+
+const astConverter = new ParserToASTConverter();
+const bot = astConverter.visit(tree);
+
+console.log(bot);
